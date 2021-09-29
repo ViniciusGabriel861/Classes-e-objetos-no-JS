6ccd4de6915f483ae42ba6e236bb4f2a32b79919
ViniciusGabriel861 <vinicius.gabriel050604@gmail.com>
2021-09-29 12:46:50 -0300

    subir arquivo

---
+++ aula.js
@@ -0,0 +1,23 @@
+//Criado por Vinícius Gabriel
+//criado usando POO
+//criação da classe 
+class Poligono {
+    //criação do construtor e seus atributos
+    constructor(base, altura) {
+      this.base = base;
+      this.altura = altura;
+    }
+    //criação dos métodos
+    somar(){
+        return(this.base + this.altura)
+    }
+    area(){
+        return(this.base * this.altura)
+    }
+  }
+  //meu objeto que exibirá a soma de sua base e altura, além do calculo da área.
+  quadrado = new Poligono(4, 2)
+  console.log(quadrado.somar())
+  console.log(quadrado.area())
+      
+  
